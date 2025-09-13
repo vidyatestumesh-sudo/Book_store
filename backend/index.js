442030be98366bfa050541d49c7d85b8ddc899d0
l0h1t <vidyatestumesh@gmail.com>
2025-09-13 11:00:49 +0530

    Update CORS origins and improve code formatting
    Added 'https://bookstore-frontend-9tii.onrender.com/' to allowed CORS origins and standardized code formatting with consistent semicolons and spacing for better readability.

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,30 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://book-app-frontend-tau.vercel.app'],
+    origin: [
+      'http://localhost:5173',
+      'https://bookstore-frontend-9tii.onrender.com/'  
+    ],
     credentials: true
-}))
+}));
 
-// routes
+// Routes
 const bookRoutes = require('./src/books/book.route');
-const orderRoutes = require("./src/orders/order.route")
-const userRoutes =  require("./src/users/user.route")
-const adminRoutes = require("./src/stats/admin.stats")
+const orderRoutes = require("./src/orders/order.route");
+const userRoutes = require("./src/users/user.route");
+const adminRoutes = require("./src/stats/admin.stats");
 
-app.use("/api/books", bookRoutes)
-app.use("/api/orders", orderRoutes)
-app.use("/api/auth", userRoutes)
-app.use("/api/admin", adminRoutes)
+app.use("/api/books", bookRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/admin", adminRoutes);
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
@@ -31,7 +33,9 @@ async function main() {
   });
 }
 
-main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+main()
+  .then(() => console.log("Mongodb connect successfully!"))
+  .catch(err => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
