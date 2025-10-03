0d41d72d6c8ce4813a71e8adc4d6924a29bce2b4
l0h1t <vidyatestumesh@gmail.com>
2025-10-03 18:05:26 +0530

    Clarity...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
@@ -7,25 +6,25 @@ import { AuthProvide } from './context/AuthContext'
 import { useEffect, useState } from 'react'
 import Loading from './components/Loading'
 import ScrollToTop from './components/ScrollToTop'
+import { clarity } from '@microsoft/clarity'
 
 function App() {
 
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    clarity.init("tke6pwgfvi");
 
     const timer = setTimeout(() => {
       setLoading(false);
     }, 1000);
 
-    // Cleanup timer
     return () => clearTimeout(timer);
   }, []);
 
   if (loading) {
     return <Loading />;
   }
-
 
   return (
     <>
